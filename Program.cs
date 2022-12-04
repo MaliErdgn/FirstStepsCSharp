ea5dd7449a0c63b8026dde7eb9e0d47682804879
MaliErdgn <malierdgnn@gmail.com>
2022-12-04 04:19:54 +0300

    added name generator

--- Program.cs
+++ Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
+using HtmlAgilityPack;
 
-namespace FirstSteps
+namespace FirstStepsCSharp
 {
     class Program
     {
         static void Main(string[] args) 
         {
-            ProgrammingChallenges.CalculateAgeInSeconds();
+            ProgrammingChallenges.NameGenerator();
         }     
 
         
@@ -213,7 +214,7 @@ namespace FirstSteps
                     }
                 Calculator();
             }
-            public static void CalculateAgeInSeconds()
+            public static void CalculateAgeInSeconds() //Done
             {
                 DateTime CalculateAgeInSecondsCurrentDate = DateTime.Now;
                 Console.WriteLine("Enter your birthdate in the format DD MM YYYY: ");
@@ -225,6 +226,45 @@ namespace FirstSteps
                 string[] CalculateAgeInSecondsOutput = CalculateAgeInSecondsDiffString.Split(",");
                 Console.WriteLine(CalculateAgeInSecondsOutput[0] + " seconds have passed since you were born!");
 
+            }
+            public static void NameGenerator()//Done 
+            {
+                static void nameAndSurnameList(int NameGeneratorLoopAmount) //Creates lists of names and surnames
+                {
+                    var html = @"https://www.ssa.gov/oact/babynames/decades/century.html"; //link of names list
+                    HtmlWeb web = new HtmlWeb();
+                    var htmlDoc = web.Load(html);
+                    var htmlSurname = @"https://www.thoughtco.com/most-common-us-surnames-1422656"; //link of surnames list
+                    HtmlWeb webSurname = new HtmlWeb();
+                    var htmlSurnameDoc = webSurname.Load(htmlSurname);
+                    List<string> nameList = new List<string>();
+                    List<string> surnameList = new List<string>();
+                    foreach (int i in Enumerable.Range(1, 100)) //adds the male names to the name list
+                    {
+                        var node = htmlDoc.DocumentNode.SelectSingleNode($"//*[@id=\"content\"]/section[2]/div/div[2]/table/tbody/tr[{i}]/td[2]");
+                        nameList.Add(node.InnerText);
+                    }
+                    foreach (int i in Enumerable.Range(1, 100)) //adds the female names to the name list
+                    {
+                        var node = htmlDoc.DocumentNode.SelectSingleNode($"//*[@id=\"content\"]/section[2]/div/div[2]/table/tbody/tr[{i}]/td[4]");
+                        nameList.Add(node.InnerText);
+                    }
+                    foreach (int i in Enumerable.Range(1, 100)) //adds the surnames to the name list
+                    {
+                        var node = htmlSurnameDoc.DocumentNode.SelectSingleNode($"//*[@id=\"mntl-sc-block_1-0-5\"]/div/table/tbody/tr[{i}]/td[2]/p");
+                        surnameList.Add(node.InnerText);
+                    }
+                    foreach (int j in Enumerable.Range(1, NameGeneratorLoopAmount)) 
+                    {
+                        var NameGeneratorRandom = new Random();
+                        int NameGeneratorNameIndex = NameGeneratorRandom.Next(nameList.Count);
+                        int NameGeneratorSurnameIndex = NameGeneratorRandom.Next(surnameList.Count);
+                        Console.WriteLine(nameList[NameGeneratorNameIndex] + " " + surnameList[NameGeneratorSurnameIndex]);
+                    }
+                }
+                Console.WriteLine("How many random names do you want? ");
+                var NameGeneratorAmount = Console.ReadLine(); //The amount of times the names will be generated     
+                nameAndSurnameList(int.Parse(NameGeneratorAmount));         
             }
         }
     }
